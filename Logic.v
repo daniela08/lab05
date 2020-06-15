d4089807ec9f5935775f5529a131325fde0eb4be
daniela08 <danielacauti@gmail.com>
2020-06-14 21:39:12 -0500

    LOgic

--- Logic.v
+++ Logic.v
@@ -8,25 +8,26 @@ module Logic(A, B, ALUop, LogicOut, out_ALUop4, out_ALUop5, out_ALUop6, out_ALUo
     output[31:0] LogicOut;
     reg[31:0] out_ALUop4, out_ALUop5, out_ALUop6, out_ALUop7;
 
-And32bit AND (.A([31:0] A),
-              .B([31:0] B),
-              .out_ALUop4([31:0] out_ALUop4) );
+And32bit AND (.A(A),
+              .B(B),
+              .out_ALUop4(out_ALUop4) );
 
-Or32bit OR (.A([31:0] A),
-            .B([31:0] B),
-            .out_ALUop5([31:0] out_ALUop5) );
+Or32bit OR (.A(A),
+            .B(B),
+            .out_ALUop5(out_ALUop5) );
 
-Xor32bit XOR (.A([31:0] A),
-              .B([31:0] B),
-              .out_ALUop6([31:0] out_ALUop6) );
+Xor32bit XOR (.A(A),
+              .B(B),
+              .out_ALUop6(out_ALUop6) );
 
-Not32bit NOR (.X([31:0] out_ALUop5),
-              .Y([31:0] out_ALUop7) );
+Not32bit NOR (.X(out_ALUop5),
+              .Y(out_ALUop7) );
 
-MUX_4_1 LogicOut (.a([31:0] out_ALUop4),
-                  .b([31:0] out_ALUop5),
-                  .c([31:0] out_ALUop6),
-                  .d([31:0] out_ALUop7),
-                  .z([31:0] LogicOut) );
+MUX_4_1 LogicOut (.a(out_ALUop4),
+                  .b(out_ALUop5),
+                  .c(out_ALUop6),
+                  .d(out_ALUop7),
+                  .ALUop([1:0] ALUop),
+                  .z(LogicOut) );
 
 endmodule
