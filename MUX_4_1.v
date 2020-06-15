5e945022274ec04c015689db2f94bde1abee74eb
miyulo139 <55298493+miyulo139@users.noreply.github.com>
2020-06-14 21:54:28 -0500

    mux 4:1

---
+++ MUX_4_1.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    21:45:30 06/14/2020 
+// Design Name: 
+// Module Name:    MUX_4_1 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module MUX_4_1(
+    );
+
+
+endmodule
