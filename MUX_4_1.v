a01a23f13c013ba1068075cb3e783b3e06018e51
miyulo139 <55298493+miyulo139@users.noreply.github.com>
2020-06-14 22:20:49 -0500

    MUX 4:1

--- MUX_4_1.v
+++ MUX_4_1.v
@@ -1,25 +1,17 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    21:45:30 06/14/2020 
-// Design Name: 
 // Module Name:    MUX_4_1 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
 //////////////////////////////////////////////////////////////////////////////////
-module MUX_4_1(
-    );
 
-
+module MUX_4_1(input [31:0]a,b,c,d,
+					input [1:0]ALUcontrol,
+					output reg[31:0]z); 
+	 always @(a,b,c,d,ALUcontrol)
+				 case (ALUcontrol)
+				 0: z <=a; //send 0 signal out
+				 1: z <=b; //send 2 signal out
+				 2: z <=c; //send 4 signal out
+				 3: z <=d; //send 5 signal out
+				 default: z <= "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ";
+				 endcase
 endmodule
